e6465faf0ad72590797b5e6623e459f377c871aa
Sudharsan Balasubramani <sudharsan.bala@yahoo.com>
2021-04-15 13:42:03 -0700

    try 2 netlify

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import $ from 'jquery' //ES6 syntax. More like my style
-import './style.scss'   // So that webpack understands to include the style. Webpack won't include anything not used by app directly.
+import $ from 'jquery'; // ES6 syntax. More like my style
+import './style.scss'; // So that webpack understands to include the style. Webpack won't include anything not used by app directly.
 
 // updates #main elt every second to how many seconds you've been on for
-let seconds = 0
+let seconds = 0;
 setInterval(() => {
-    $('#main').html(`You've been on this page for ${++seconds} seconds.`)
+  $('#main').html(`You've been on this page for ${++seconds} seconds.`);
 }, 1000);
 
-console.log("Hello World");
+console.log('Hello World');
