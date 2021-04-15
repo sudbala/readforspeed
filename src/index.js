49cadff386a7b7be8ed546ba4b1ecc86fe2ebdc4
Sudharsan Balasubramani <sudharsan.bala@yahoo.com>
2021-04-15 11:54:14 -0700

    second-tracking page implemented

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('starting up!')
+import $ from 'jquery' //ES6 syntax. More like my style
+
+// updates #main elt every second to how many seconds you've been on for
+let seconds = 0
+setInterval(() => {
+    $('#main').html(`You've been on this page for ${++seconds} seconds.`)
+}, 1000);
