85aa7e47a31fa1642f1e578795d08a7ce0dd51f2
Sudharsan Balasubramani <sudharsan.bala@yahoo.com>
2021-04-18 11:42:49 -0700

    react support to starter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import $ from 'jquery'; // ES6 syntax. More like my style
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 // So that webpack understands to include the style. Webpack won't include anything not used by app directly.
 
-// updates #main elt every second to how many seconds you've been on for
-let seconds = 0;
-setInterval(() => {
-  seconds += 1;
-  $('#main').html(`You've been on this page for ${seconds} seconds.`);
-}, 1000);
+const App = () => <div className="test">All the REACT are belong to us</div>;
 
-console.log('Hello World');
+ReactDOM.render(<App />, document.getElementById('main'));
+
+
+
+
+// // updates #main elt every second to how many seconds you've been on for
+// let seconds = 0;
+// setInterval(() => {
+//   seconds += 1;
+//   $('#main').html(`You've been on this page for ${seconds} seconds.`);
+// }, 1000);
+
+// console.log('Hello World');
